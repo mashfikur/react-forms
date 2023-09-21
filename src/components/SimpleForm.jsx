1fd7c9052ed40301bec6248350d93cd02f082cb1
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-21 13:51:38 +0600

    simple form structure created

--- src/components/SimpleForm.jsx
+++ src/components/SimpleForm.jsx
@@ -1,9 +1,18 @@
 const SimpleForm = () => {
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value)
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={handleSubmit} >
+        <input type="text" name="text" />
         <br />
+        <input type="email" name="email" />
+        <br />
+        {/* <input type="submit" value="Submit" /> */}
         <button>Submit</button>
       </form>
     </div>
